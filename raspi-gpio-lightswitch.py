30690cbb47c539fbbb3b1b98efc88da675ea38b0
Michael Heise <mh.mh@web.de>
2021-06-28 19:41:16 +0200

    switch to Raspi GPIO Zero based functions define main program class handle external pull resistors

--- raspi-gpio-lightswitch.py
+++ raspi-gpio-lightswitch.py
@@ -3,8 +3,8 @@
 __author__ = "Michael Heise"
 __copyright__ = "Copyright (C) 2021 by Michael Heise"
 __license__ = "LGPL"
-__version__ = "0.1.0"
-__date__ = "06/25/2021"
+__version__ = "0.2.0"
+__date__ = "06/28/2021"
 
 """Configurable python service to run on Raspberry Pi
    and evaluate one GPIO-in to toggle one GPIO-out as light switch.
@@ -17,119 +17,126 @@ import logging
 from systemd.journal import JournalHandler
 
 # 3rd party imports
-import RPi.GPIO as GPIO
+import gpiozero
 
 # local imports
 # - none -
 
-def initLogging():
-    """ initialize logging to journal
-    """
-    global log = logging.getLogger(__name__)
-    log_fmt = logging.Formatter("%(levelname)s %(message)s")
-    logHandler = JournalHandler()
-    logHandler.setFormatter(log_fmt)
-    log.addHandler(logHandler)
-    log.setLevel(logging.INFO)
-    return
 
-def initGPIO(configGPIO):
-    """ evaluate the data read from config file to
+class RaspiGPIOLightSwitch:
+    def __init__(self):
+        self._isValidGPIO = False
+        self._CONFIGFILE = "raspi-gpio-lightswitch.conf"
+        self._valuesUpDn = ["up", "dn", "upex", "dnex"]
+        self._valuesPressRelease = ["press", "release"]
+
+    def initLogging(log):
+        """initialize logging to journal"""
+        log_fmt = logging.Formatter("%(levelname)s %(message)s")
+        logHandler = JournalHandler()
+        logHandler.setFormatter(log_fmt)
+        log.addHandler(logHandler)
+        log.setLevel(logging.INFO)
+        return
+
+    def initGPIO(configGPIO):
+        """evaluate the data read from config file to
         set the GPIO input and output
-    """
-    GPIO.setmode(GPIO.BCM)
-    
-    log.info("Button configuration = '{0}'".format(configGPIO["Button"]))
-        
-    buttonConfig = configGPIO["Button"].split(",")
-    pudStr = buttonConfig[1].lower()
-    if pudStr == "up":
-        pud = GPIO.PUD_UP
-    elif pudStr == "dn":
-        pud = GPIO.PUD_DOWN
-    else
-        log.error("Invalid resistor configuration! Only 'UP' or 'DN' allowed!")
-        return False
-            
-    edgeStr = buttonConfig[2].lower()
-    if edgeStr == "fall":
-        edge = GPIO.FALLING
-    elif edgeStr == "rise":
-        edge = GPIO.RISING
-    else
-        log.error("Invalid edge configuration! Only 'RISE' or 'FALL' allowed!")
-        return False
-        
-    try:
-        if buttonConfig.count()=4:
-            bouncetime = int(buttonConfig[3])
+        """
+        log.info("Button configuration = '{0}'".format(configGPIO["Button"]))
+
+        buttonConfig = configGPIO["Button"].split(",")
+
+        pudStr = buttonConfig[1].lower()
+        if not pudStr in valuesUpDn:
+            log.error("Invalid resistor configuration! Only 'UP' or 'DN' allowed!")
+            return False
+
+        pud = None
+        active = None
+        if pudStr == "up":
+            pud = True
+        if pudStr == "dn":
+            pud = False
+        if pudStr == "upex":
+            active = False
+        if pudStr == "dnex":
+            active = True
+
+        eventStr = buttonConfig[2].lower()
+        if not eventStr in valuesPressRelease:
+            log.error("Invalid event configuration! Only 'PRESS' or 'RELEASE' allowed!")
+            return False
+
+        event = True if eventStr == "press" else False
+
+        try:
+            if len(buttonConfig) == 4:
+                bouncetime = int(buttonConfig[3])
+            else:
+                bouncetime = 200
+        except:
+            log.error("Invalid bounce time! (only integer >0 allowed)")
+            return false
+
+        try:
+            self._button = gpiozero.Button(
+                int(buttonConfig[0]),
+                pull_up=pud,
+                active_state=active,
+                bouncetime=bouncetime,
+            )
+            if event:
+                self._button.when_pressed = toggleLight
+            else:
+                self._button.when_released = toggleLight
+        except:
+            log.error("Error while setting up GPIO input for button!")
+            return False
+
+        log.info("Light configuration = '{0}'".format(configGPIO["Light"]))
+
+        try:
+            lightConfig = int(configGPIO["Light"])
+            self._light = gpiozero.LED(lightConfig)
+        except:
+            log.info("Error while setting up GPIO output for light!")
+            return False
+
+        self._isValidGPIO = True
+        return True
+
+    def switchLight(state):
+        """switch the output pin to the desired state"""
+        self._lightonoff = state
+        if state:
+            self._light.on()
         else:
-            bouncetime = 200
-    except:
-        log.error("Invalid bounce time! (only integer >0 allowed)")
-        return false
+            self._light.off()
+        log.info("Light was set to {0}.".format(getOnOffStr(self._lightonoff)))
+        return
 
-    try:
-        GPIO.setup(buttonConfig[0], GPIO.IN, pull_up_down=pud)
-        GPIO.add_event_detect(int(buttonConfig[0]), edge, callback=toggleLight, bouncetime=bouncetime)
-    except:
-        log.error("Error while setting up GPIO input for button!")
-        return False
+    def toggleLight():
+        """toggle the output pin"""
+        log.info(
+            "Callback - toggle light output from {0} to {1}...".format(
+                getOnOffStr(self._lightonoff), getOnOffStr(not self._lightonoff)
+            )
+        )
+        self._light.Toggle()
+        self._lightonoff = self._light.is_lit
+        return
 
-    log.info("Light configuration = '{0}'".format(configGPIO["Light"]))
-    
-    try:
-        lightConfig = int(configGPIO["Light"])
-        GPIO.setup(lightConfig, GPIO.OUT)
-    except:
-        log.info("Error while setting up GPIO output for button!")
-        return False
-    
-    global isValidGPIO = True
-    return True
+    def getOnOffStr(state):
+        return "On" if state else "Off"
 
-def cleanupGPIO():
-    """ clean the used GPIO pins
-    """
-    if log:
-        log.info("Cleaning up GPIO resources.")
-    GPIO.cleanup()
-    global isValidGPIO = False
-    return
-
-def switchLight(state):
-    """ switch the output pin to the desired state
-    """
-    global lightonoff = state
-    GPIO.output(lightConfig, lightonoff)
-    log.info("Light was set to {0}.".format(getOnOffStr(lightonoff)))
-    return
-
-def toggleLight():
-    """ toggle the output pin
-    """
-    log.info("Callback - toggle light output from {0} to {1}...".format(getOnOffStr(lightonoff),
-                                                                        getOnOffStr(not lightonoff)))
-    switchLight(not lightonoff)
-    return
-
-def getOnOffStr(state):
-    return "On" if state else "Off"
-
-CONFIGFILE = "raspi-gpio-lightswitch.conf"
-
-#CRITICAL 50
-#ERROR 40
-#WARNING 30
-#INFO 20
-#DEBUG 10
-#NOTSET 0
 
 try:
-    isValidGPIO = False
-    
-    initLogging()
-    
+    log = logging.getLogger(__name__)
+
+    lightswitch = RaspiGPIOLightswitch()
+    lightswitch.initLogging(log)
+
     log.info("Reading configuration file...")
     try:
         config = configparser.ConfigParser()
@@ -142,16 +149,17 @@ try:
         log.error("Invalid configuration file! (No [GPIO] section)")
         sys.exit(-3)
 
-    log.info("Init GPIO configuration:")
-    if not initGPIO(config["GPIO"]):
+    log.info("Init GPIO configuration.")
+    if not lightswitch.initGPIO(config["GPIO"]):
         sys.exit(-3)
-    
-    log.info("Setting defined inital off state.")
-    switchLight(False)
-    
+
+    # log.info("Setting defined inital off state.")
+    # lightswitch.switchLight(False)
+
     log.info("Starting service loop...")
     while True:
-        
+        pass
+
 except Exception as e:
     if log:
         log.exception("Unhandled exception: {0}".format(e.args[0]))
@@ -160,5 +168,4 @@ finally:
     if isValidGPIO:
         if log:
             log.info("Finally setting output to off state.")
-        switchLight(False)
-    cleanupGPIO()
+        lightswitch.switchLight(False)
