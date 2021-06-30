dbf8487c64ca22c85a81276fea78023a44183da6
Michael Heise <mh.mh@web.de>
2021-06-30 18:59:41 +0200

    *bug-fix* add many missing self references for fields and method signatures *bug-fix* correct typo in class instance creation of RaspiGPIOLightSwitch *bug-fix* correct typo for bounce_time argument *bug-fix* correct typo for toggle() method add handler for SIGTERM signal

--- raspi-gpio-lightswitch.py
+++ raspi-gpio-lightswitch.py
@@ -3,16 +3,17 @@
 __author__ = "Michael Heise"
 __copyright__ = "Copyright (C) 2021 by Michael Heise"
 __license__ = "LGPL"
-__version__ = "0.2.1"
-__date__ = "06/29/2021"
+__version__ = "0.2.2"
+__date__ = "06/30/2021"
 
 """Configurable python service to run on Raspberry Pi
-   and evaluate one GPIO-in to toggle one GPIO-out as light switch.
+   and evaluate one GPIO-in to control one GPIO-out as light switch.
 """
 
 # standard imports
 import configparser
 import sys
+import signal
 import logging
 from systemd.journal import JournalHandler
 
@@ -25,44 +26,47 @@ import gpiozero
 
 class RaspiGPIOLightSwitch:
     def __init__(self):
-        self._isValidGPIO = False
+        self.isValidGPIO = False
         self.CONFIGFILE = "raspi-gpio-lightswitch.conf"
         self.valuesUpDn = ["up", "dn", "upex", "dnex"]
         self.valuesPressRelease = ["press", "release"]
         self.config = None
 
-    def initLogging(log):
+    def initLogging(self, log):
         """initialize logging to journal"""
         log_fmt = logging.Formatter("%(levelname)s %(message)s")
         logHandler = JournalHandler()
         logHandler.setFormatter(log_fmt)
         log.addHandler(logHandler)
         log.setLevel(logging.INFO)
+        self._log = log
         return
 
-    def readConfigFile():
+    def readConfigFile(self):
         """read the config file"""
         try:
             self.config = configparser.ConfigParser()
             self.config.read(self.CONFIGFILE)
             return True
         except Exception:
-            log.error("Accessing config file '{0}' failed!", CONFIGFILE)
+            self._log.error("Accessing config file '{0}' failed!", CONFIGFILE)
             return False
 
-    def initGPIO():
+    def initGPIO(self):
         """evaluate the data read from config file to
         set the GPIO input and output
         """
         configGPIO = self.config["GPIO"]
 
-        log.info("Button configuration = '{0}'".format(configGPIO["Button"]))
+        self._log.info("Button configuration = '{0}'".format(configGPIO["Button"]))
 
         buttonConfig = configGPIO["Button"].split(",")
 
         pudStr = buttonConfig[1].lower()
-        if not pudStr in valuesUpDn:
-            log.error("Invalid resistor configuration! Only 'UP' or 'DN' allowed!")
+        if not pudStr in self.valuesUpDn:
+            self._log.error(
+                "Invalid resistor configuration! Only 'UP' or 'DN' allowed!"
+            )
             return False
 
         pud = None
@@ -77,8 +81,10 @@ class RaspiGPIOLightSwitch:
             active = True
 
         eventStr = buttonConfig[2].lower()
-        if not eventStr in valuesPressRelease:
-            log.error("Invalid event configuration! Only 'PRESS' or 'RELEASE' allowed!")
+        if not eventStr in self.valuesPressRelease:
+            self._log.error(
+                "Invalid event configuration! Only 'PRESS' or 'RELEASE' allowed!"
+            )
             return False
 
         event = True if eventStr == "press" else False
@@ -89,7 +95,7 @@ class RaspiGPIOLightSwitch:
             else:
                 bouncetime = 200
         except:
-            log.error("Invalid bounce time! (only integer >0 allowed)")
+            self._log.error("Invalid bounce time! (only integer >0 allowed)")
             return false
 
         try:
@@ -97,57 +103,73 @@ class RaspiGPIOLightSwitch:
                 int(buttonConfig[0]),
                 pull_up=pud,
                 active_state=active,
-                bouncetime=bouncetime,
+                bounce_time=0.001 * bouncetime
             )
             if event:
-                self._button.when_pressed = toggleLight
+                self._button.when_pressed = self.toggleLight
             else:
-                self._button.when_released = toggleLight
+                self._button.when_released = self.toggleLight
         except:
-            log.error("Error while setting up GPIO input for button!")
+            self._log.error("Error while setting up GPIO input for button!")
             return False
 
-        log.info("Light configuration = '{0}'".format(configGPIO["Light"]))
+        self._log.info("Light configuration = '{0}'".format(configGPIO["Light"]))
 
         try:
             lightConfig = int(configGPIO["Light"])
             self._light = gpiozero.LED(lightConfig)
         except:
-            log.info("Error while setting up GPIO output for light!")
+            self._log.error("Error while setting up GPIO output for light!")
             return False
 
-        self._isValidGPIO = True
+        self._lightonoff = False
+
+        self.isValidGPIO = True
         return True
 
-    def switchLight(state):
+    def switchLight(self, state):
         """switch the output pin to the desired state"""
-        self._lightonoff = state
+        self._log.debug("Switch light {0}".format(self.getOnOffStr(state)))
         if state:
             self._light.on()
         else:
             self._light.off()
-        log.info("Light was set to {0}.".format(getOnOffStr(self._lightonoff)))
+        self._lightonoff = self._light.is_lit
+        self._log.debug("Light is {0}.".format(self.getOnOffStr(self._lightonoff)))
         return
 
-    def toggleLight():
+    def toggleLight(self):
         """toggle the output pin"""
-        log.info(
+        self._log.debug(
             "Callback - toggle light output from {0} to {1}...".format(
-                getOnOffStr(self._lightonoff), getOnOffStr(not self._lightonoff)
+                self.getOnOffStr(self._lightonoff),
+                self.getOnOffStr(not self._lightonoff),
             )
         )
-        self._light.Toggle()
+        self._light.toggle()
         self._lightonoff = self._light.is_lit
+        self._log.debug("Light is {0}.".format(self.getOnOffStr(self._lightonoff)))
         return
 
-    def getOnOffStr(state):
+    def getOnOffStr(self, state):
         return "On" if state else "Off"
 
+
+def sigterm_handler(_signo, _stack_frame):
+    """clean exit on SIGTERM signal (when systemd stops the process)
+    """
+    sys.exit(0)
+
+# install handler
+signal.signal(signal.SIGTERM, sigterm_handler)
+
+log = None
+lightswitch = None
 
 try:
     log = logging.getLogger(__name__)
 
-    lightswitch = RaspiGPIOLightswitch()
+    lightswitch = RaspiGPIOLightSwitch()
     lightswitch.initLogging(log)
 
     log.info("Reading configuration file...")
@@ -171,7 +193,7 @@ except Exception as e:
         log.exception("Unhandled exception: {0}".format(e.args[0]))
     sys.exit(-1)
 finally:
-    if isValidGPIO:
+    if lightswitch and lightswitch.isValidGPIO:
         if log:
             log.info("Finally setting output to off state.")
         lightswitch.switchLight(False)
