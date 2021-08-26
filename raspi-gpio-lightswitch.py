c99c51926cdbe8109244546d4ca5ddd23a979764
Michael Heise <mh.mh@web.de>
2021-08-26 19:01:02 +0200

    start implementation of dimming options add gpiozero_pin_factory.conf

--- raspi-gpio-lightswitch.py
+++ raspi-gpio-lightswitch.py
@@ -3,8 +3,8 @@
 __author__ = "Michael Heise"
 __copyright__ = "Copyright (C) 2021 by Michael Heise"
 __license__ = "Apache License Version 2.0"
-__version__ = "0.2.3"
-__date__ = "07/03/2021"
+__version__ = "0.3.1"
+__date__ = "08/24/2021"
 
 """Configurable python service to run on Raspberry Pi
    and evaluate one GPIO-in to control one GPIO-out as light switch.
@@ -45,9 +45,12 @@ class RaspiGPIOLightSwitch:
         
         self.isValidGPIO = False
         self.CONFIGFILE = "/etc/raspi-gpio-lightswitch.conf"
-        self.valuesUpDn = ["up", "dn", "upex", "dnex"]
+        self.STATEFILE = "~/raspi-gpio-lightswitch/raspi-gpio-lightswitch.state"
+        self.valuesPullUpDn = ["up", "dn", "upex", "dnex"]
         self.valuesPressRelease = ["press", "release"]
+        self.valuesDimUpDn = ["up", "dn"]
         self.config = None
+        self.stored_value = 1.0
 
     def remove(self):
         self._finalizer()
@@ -85,7 +88,7 @@ class RaspiGPIOLightSwitch:
             return False
 
     def initGPIO(self):
-        """evaluate the data read from config file to
+        """evaluate the data read from the config file to
         set the GPIO input and output
         """
         self._log.info("Init GPIO configuration.")
@@ -96,7 +99,7 @@ class RaspiGPIOLightSwitch:
         buttonConfig = configGPIO["Button"].split(",")
 
         pudStr = buttonConfig[1].lower()
-        if not pudStr in self.valuesUpDn:
+        if not pudStr in self.valuesPullUpDn:
             self._log.error(
                 "Invalid resistor configuration! Only 'UP' or 'DN' allowed!"
             )
@@ -106,11 +109,11 @@ class RaspiGPIOLightSwitch:
         active = None
         if pudStr == "up":
             pud = True
-        if pudStr == "dn":
+        elif pudStr == "dn":
             pud = False
-        if pudStr == "upex":
+        elif pudStr == "upex":
             active = False
-        if pudStr == "dnex":
+        elif pudStr == "dnex":
             active = True
 
         eventStr = buttonConfig[2].lower()
@@ -148,9 +151,63 @@ class RaspiGPIOLightSwitch:
 
         self._log.info("Light configuration = '{0}'".format(configGPIO["Light"]))
 
+        if configGPIO["Dim"]:
+            self._log.info("Dimming configuration = '{0}'".format(configGPIO["Dim"]))
+
+            try:
+                dimConfig = configGPIO["Dim"].split()
+                self._dimMode = int(dimConfig[0])
+                
+                if self._dimMode > 2 || self._dimMode < 0:
+                    raise
+                
+                if ((self._dimMode == 0 && len(dimConfig) > 1) ||
+                    (len(dimConfig) > self._dimMode + 2)
+                    ):
+                    raise
+                    
+                if len(dimConfig) > 1:
+                    self._dimStep = 1.0 / (int(dimConfig(1)) + 1)
+                else:
+                    self._dimStep = 4
+
+                if len(dimConfig) > 2:
+                    if dimConfig(2).lower() = "dn":
+                        self._dimStep = -self._dimStep
+                    elif dimConfig(2).lower() != "up":
+                        raise
+                    
+                if len(dimConfig) > 3:
+                    self._dimHoldSec = int(dimConfig(3))
+                else:
+                    self._dimHoldSec = 2
+            except:
+                self._log.error("Error in dimming configuration!")
+                return False
+
+            try:
+                if self._dimMode == 2:
+                    self._button.hold_time = self._dimHoldSec
+                    self._button.hold_repeat = True
+                    self._button.when_held = self.dimLight
+                elif self._dimMode == 1:
+                    if event:
+                        self._button.when_pressed = self.dimLight
+                    else
+                        self._button.when_released = self.dimLight
+            except:
+                self._log.error("Error while setting up dimming!")
+                return False
+        else:
+            self._dimMode = 0
+            self._dimStep = 0
+        
         try:
             lightConfig = int(configGPIO["Light"])
-            self._light = gpiozero.LED(lightConfig)
+            if self._dimMode == 0:
+                self._light = gpiozero.LED(lightConfig)
+            else:
+                self._light = gpiozero.PWMLED(lightConfig)
         except:
             self._log.error("Error while setting up GPIO output for light!")
             return False
@@ -160,6 +217,7 @@ class RaspiGPIOLightSwitch:
         self.isValidGPIO = True
         return True
 
+
     def switchLight(self, state):
         """switch the output pin to the desired state"""
         self._log.debug("Switch light {0}".format(self.getOnOffStr(state)))
@@ -168,21 +226,56 @@ class RaspiGPIOLightSwitch:
         else:
             self._light.off()
         self._lightonoff = self._light.is_lit
-        self._log.debug("Light is {0}.".format(self.getOnOffStr(self._lightonoff)))
-        return
+        self._log.debug("Light is {0} now.".format(self.getOnOffStr(self._lightonoff)))
+
 
     def toggleLight(self):
         """toggle the output pin"""
         self._log.debug(
-            "Callback - toggle light output from {0} to {1}...".format(
+            "Toggle light output from {0} to {1}...".format(
                 self.getOnOffStr(self._lightonoff),
                 self.getOnOffStr(not self._lightonoff),
             )
         )
-        self._light.toggle()
+        if self._lightonoff:
+            self._light.off()
+        else:
+            self._light.value = self.stored_value
+            
         self._lightonoff = self._light.is_lit
-        self._log.debug("Light is {0}.".format(self.getOnOffStr(self._lightonoff)))
-        return
+        if self._lightonoff:
+            if self._dimMode > 0:
+                self._log.debug("Light is on now at {0}%.".format(100 * self._light.value))
+            else
+                self._log.debug("Light is on now.")
+        else
+            self._log.debug("Light is off now.")
+
+
+    def dimLight(self):
+        """dim the light up/dn using configured settings"""
+        self._log.debug(
+            "Dim light {0}.".format("up" if self._dimStep > 0 else "dn")
+            )
+
+        next_value = self._light.value + self._dimStep
+        if next_value < 0:
+            next_value = 1
+
+        if next_value > 1:
+            if self._dimMode == 1:
+                next_value = 0
+            else
+                next_value = self._dimStep
+
+        self._light.value = next_value
+        self.stored_value = next_value
+        self._lightonoff = self._light.is_lit
+        if self._lightonoff:
+            self._log.debug("Light is on now at {0}%.".format(100 * self._light.value))
+        else
+            self._log.debug("Light is off now.")
+
 
     def getOnOffStr(self, state):
         return "On" if state else "Off"
